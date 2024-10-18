ffbdefebf2e3739cb9ad1d1fa2e4819c03ef3a95
Aiimey <132620366+Aiimeey@users.noreply.github.com>
2024-10-19 00:29:06 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- list all bands with `Glam rock` as their main style
--- Column band_name, lifespan
+-- list all bands with `Glam rock` as their main style 
+-- (YEAR(CURDATE()))
 
 SELECT band_name, (IF(split IS NULL, 2022, split) - formed) AS lifespan
 FROM metal_bands
